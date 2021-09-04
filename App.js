1fa5240175f913b854f1ed64c3a0c9c1096c9a11
justDenzil <vikash.denzil@gmail.com>
2021-09-04 20:56:59 +0530

    date/time

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+import dateTime from './components/dateTime';
 
 const bg = require('./assets/bg.png')
 
@@ -8,7 +8,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <ImageBackground source={bg} style={styles.bg}>
-        <StatusBar style="auto" />
+        <dateTime />
       </ImageBackground>
     </View>
   );
