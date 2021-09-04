b6f420b1c272e5b865b231d30d1cf974aa4de9df
justDenzil <vikash.denzil@gmail.com>
2021-09-04 19:17:10 +0530

    bg

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+
+const bg = require('./assets/bg.png')
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ImageBackground source={bg} style={styles.bg}>
+        <StatusBar style="auto" />
+      </ImageBackground>
     </View>
   );
 }
@@ -14,8 +17,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  bg: {
+    flex:1,
+    resizeMode:"cover",
+    justifyContent:"center",
+  }
 });
